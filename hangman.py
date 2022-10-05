1ce274e09bd7f6bc048c09e06f49f04fa17751fc
Alec Hansen <alecdhansen@Alecs-Air.lan>
2022-10-04 23:19:20 -0400

    game complete, colors added

--- hangman.py
+++ hangman.py
@@ -1,40 +1,90 @@
 from random import choice
+import time
+import colorama
+from colorama import Fore, Back, Style
+from random_word import RandomWords
 
+r = RandomWords()
+word = r.get_random_word()
 print(
+    Fore.CYAN,
     """
-----------Let's Play a Game!----------
-   _    _    _    _    _    _    _  
-  / \  / \  / \  / \  / \  / \  / \ 
- ( H )( a )( n )( g )( m )( a )( n )
-  \_/  \_/  \_/  \_/  \_/  \_/  \_/ 
-
-"""
+-----Let's Play a Game!-----
+             __              
+|__| /\ |\ |/ _`|\/| /\ |\ | 
+|  |/~~\| \|\__>|  |/~~\| \|                             
+""",
 )
 
-word_list = [
-    "tree",
-    "tower",
-    "bubble",
-    "fortress",
-    "ivory",
-    "images",
-    "park",
-    "marriage",
-    "clone",
-    "shake",
-]
-word = choice(word_list)
-
-print("Here's your word!")
-for i in word:
-    print("_", end=" ")
-
-play = True
-chances = len(word) + 2
-correct_guesses = 0
-flag = 0
 
 def hangman(word):
-    # while play == True chances != 0
+    print(Fore.RESET, "I've thought of a word!")
+    time.sleep(1.4)
+    print()
+    display = []
+    for i in range(len(word)):
+        display.append("_")
+    print("It contains", len(word), "letters.")
+    time.sleep(1.2)
+    print()
+    play = True
+    chances = 8
+    wrong_letters = []
+    all_guessed_letters = []
 
-    # break
+    print("You will start with", chances, "lives, good luck!")
+    time.sleep(1.7)
+    print()
+    while play == True:
+        guess = input(Fore.YELLOW + "Guess a letter! ").lower()
+        if guess == "stop":
+            break
+        time.sleep(0.5)
+        print(Fore.RESET)
+        if guess in wrong_letters:
+            print(Fore.RED, "You already guessed", guess, "! Try again!")
+        elif guess not in word:
+            print(Fore.RED, Style.BRIGHT, guess, "is not in the word I'm thinking of.")
+            print(Fore.RESET, Style.RESET_ALL)
+            chances = chances - 1
+            wrong_letters.append(guess)
+        index = 0
+        for char in word:
+            if char == guess:
+                display[index] = guess
+                if guess in display and not wrong_letters:
+                    print(Fore.GREEN, Style.BRIGHT, "You guessed a letter right!")
+                elif guess in display and wrong_letters:
+                    print(
+                        Style.DIM,
+                        Fore.GREEN,
+                        "You already got that one right!",
+                        Style.RESET_ALL,
+                    )
+            index = index + 1
+        print(Fore.RESET, "".join(display))
+        print("Lives:", chances)
+        print()
+        if "_" not in display:
+            print(Fore.GREEN, Back.GREEN, "You win! Nicely done!")
+            play = False
+        elif chances == 0:
+            print(Fore.RED, Back.BLACK, Style.BRIGHT, "You lose!")
+            time.sleep(0.5)
+            print("The word was", word, "!")
+            play = False
+
+        # end_of_game = True
+        # while end_of_game:
+        #     answer = input(
+        #         "If you'd like to play again, enter 'yes', otherwise enter 'no'."
+        #     ).lower()
+        #     if answer == "yes":
+        #         hangman()
+        #     elif answer == "no":
+        #         end_of_game = False
+        #     else:
+        #         print("Please answer 'yes' or 'no'.")
+
+
+hangman(word)
